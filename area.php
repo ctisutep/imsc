a5be0ba322a46bb64c008c97a3061fa8d8fa62ec
ulises <ulimartinez96@gmail.com>
2017-07-06 10:43:46 -0600

    Added area.php for testing the rectangles

---
+++ area.php
@@ -0,0 +1,123 @@
+<style>
+/* Always set the map height explicitly to define the size of the div
+* element that contains the map. */
+
+#map {
+  height: 100%;
+}
+
+
+/* Optional: Makes the sample page fill the window. */
+
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#floating-panel {
+  position: absolute;
+  top: 10px;
+  left: 25%;
+  z-index: 5;
+  background-color: #fff;
+  padding: 5px;
+  border: 1px solid #999;
+  text-align: center;
+  font-family: 'Roboto', 'sans-serif';
+  line-height: 30px;
+  padding-left: 10px;
+}
+
+</style>
+<div id="map"></div>
+<div id="floating-panel">
+  <input onclick="removeRectangle();" type=button value="Remove rectangle">
+</div>
+<!-- Replace the value of the key parameter with your own API key. -->
+<!--<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&callback=initMap"> -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCY0B3_Fr1vRpgJDdbvNmrVyXmoOOtiq64&libraries=drawing&callback=initMap"async defer></script>
+
+
+</script>
+
+<script>
+// This example adds a user-editable rectangle to the map.
+// When the user changes the bounds of the rectangle,
+// an info window pops up displaying the new bounds.
+
+var rectangle;
+var map;
+var infoWindow;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 44.5452,
+      lng: -78.5389
+    },
+    zoom: 9
+  });
+
+  var bounds = {
+    north: 44.599,
+    south: 44.490,
+    east: -78.443,
+    west: -78.649
+  };
+
+  // Define the rectangle and set its editable property to true.
+  rectangle = new google.maps.Rectangle({
+    bounds: bounds,
+    editable: true,
+    draggable: true,
+    clickable: true
+  });
+
+  rectangle.setMap(map);
+
+  // Add an event listener on the rectangle.
+  rectangle.addListener('bounds_changed', showNewRect);
+  rectangle.addListener('click', clickRect);
+
+  // Define an info window on the map.
+  infoWindow = new google.maps.InfoWindow();
+}
+// Show the new coordinates for the rectangle in an info window.
+
+/** @this {google.maps.Rectangle} */
+function showNewRect(event) {
+  var ne = rectangle.getBounds().getNorthEast();
+  var sw = rectangle.getBounds().getSouthWest();
+
+  var contentString = '<b>Rectangle moved.</b><br>' +
+  'New north-east corner: ' + ne.lat() + ', ' + ne.lng() + '<br>' +
+  'New south-west corner: ' + sw.lat() + ', ' + sw.lng();
+
+  // Set the info window's content and position.
+  infoWindow.setContent(contentString);
+  infoWindow.setPosition(ne);
+
+  infoWindow.open(map);
+}
+
+function clickRect(event) {
+  var ne = rectangle.getBounds().getNorthEast();
+  var sw = rectangle.getBounds().getSouthWest();
+  var center = rectangle.getBounds().getCenter();
+
+  var contentString = '<b>Rectangle clicked.</b><br>';
+
+  // Set the info window's content and position.
+  infoWindow.setContent(contentString);
+  infoWindow.setPosition(center);
+
+  infoWindow.open(map);
+}
+
+function removeRectangle() {
+  rectangle.setMap(null);
+}
+
+</script>
