e04fc0892889e6f00ac7f37aace5bdf5184543f0
ulises <ulimartinez96@gmail.com>
2017-07-25 08:36:28 -0600

    Working on: cleaning up the legend, making squares for the rest of the colors. To-do: speed, add more filters (ex. color only pi > x_value), dynamic legend, make text more intuitive for the user, alphabetize properties

--- index2.php
+++ index2.php
@@ -955,9 +955,11 @@ function getPolygons(){//this is run button
 							<img src='img/neonpurplesquare.png' height='10px'/> A-8 (80 to 85)<br>\
 							<img src='img/graysquare.png' height='10px'/> Not rated or not available ";
 							var a = parseFloat(data.coords[key][app.payload.property]);
+							console.log(a);
 							switch (true) {
-								case (a >= 0 &&  a < 5): // LESS THAN OR EQUAL TO 0
-								colorSelect = 1;
+								case (a >= 0 && a < 5): // LESS THAN OR EQUAL TO 0
+								console.log('hey');
+								colorSelector = 1;
 								newzIndex = 1;
 								break;
 								case (a >= 5 && a < 10): // BETWEEN 21 AND 40
