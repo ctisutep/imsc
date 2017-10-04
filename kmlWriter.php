14d6464c686f42e22eace621735ae3bac19b513c
ulises <ulimartinez96@gmail.com>
2017-10-04 13:11:58 -0600

    Done: secure. Doing: kml exporter.To-do: work with Dr. Nazarian to show accurate descriptions, implement KMZ/KML exporter

---
+++ kmlWriter.php
@@ -0,0 +1,67 @@
+<?php
+//$prop = $_GET['0'];
+//$prop2 = $_GET['1'];
+$length = $_GET['length'];
+
+$string_kml = "";
+$all_poly = array();
+for ($i=0; $i < $length; $i++) { //array_push($values, $placeholder); values es array
+  array_push($all_poly, $_GET[$i]);
+}
+//var_dump($all_poly);
+if($length == 1){
+  $string_kml = "
+  <kml>
+    <Document>
+      <name>Polygon.kml</name>
+      <open>0</open>
+      <Placemark>
+        <name>hollow box 1 </name>
+        <Polygon>
+          <extrude>1</extrude>
+          <altitudeMode>relativeToGround</altitudeMode>
+          <outerBoundaryIs>
+            <LinearRing>
+              <coordinates>
+                $all_poly[0]
+              </coordinates>
+            </LinearRing>
+          </outerBoundaryIs>
+        </Polygon>
+      </Placemark>
+    </Document>
+  </kml>";
+}
+else{ //multiple tags/polygons
+
+}
+$filename = "php_example.kml";
+$file = fopen( $filename, "a+" );
+
+if( $file == false ) {
+  echo ( "Error in opening file first" );
+  exit();
+}
+if(filesize($filename)>0){ //
+  $filesize = filesize( $filename );
+  $filetext = fread( $file, $filesize );
+
+  //echo ( "File size : $filesize bytes" );
+  //echo ( "<pre>$filetext</pre>" );
+  fwrite( $file, "\n This is a sample test \n" );
+}
+else{
+  fwrite( $file,  $string_kml);
+  fclose($file);
+  $filename = "php_example.kml";
+  $file = fopen( $filename, "a+" );
+  $filesize = filesize( $filename );
+
+}
+
+fclose( $file );
+//unlink($filename);
+
+//echo $string_kml;
+//echo "<kml><coords>3.1516, 16545.111, 0</coords></kml>";
+?>
