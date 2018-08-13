dd851b1937d35562ca43312da52b01cf9904828a
ulises <jorgehuertahu@gmail.com>
2018-08-13 08:54:45 -0600

    to-do: backend for selecting counties

--- index2.php
+++ index2.php
@@ -1,10 +1,10 @@
 <?php
-//session_start();
-//if(!isset($_SESSION['in']) OR !$_SESSION['in']){
-//    header('Location: login.php');
-//    exit();
-//}
-//?>
+session_start();
+if(!isset($_SESSION['in']) OR !$_SESSION['in']){
+    header('Location: login.php');
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
