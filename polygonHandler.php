c5dbf8a8b2742f0b5251a1611405f0a2f4ac890c
Jorge Huerta <jhuerta6@miners.utep.edu>
2019-12-04 15:44:36 -0700

    added districts - testing

--- polygonHandler.php
+++ polygonHandler.php
@@ -1135,6 +1135,15 @@ function getPolygons(){
    			// SELECT * from imsc.all_pi where top > 0 and bottom <= 15.24 ORDER BY OGR_FID DESC;
 			// SELECT * from imsc.all_gypsum where top > 0 and bottom <= 15.24 ORDER BY OGR_FID DESC;
 
+			// SELECT * FROM imsc.districts;
+			// SET @geom1 = (select ST_ASTEXT(SHAPE) from imsc.districts where txdot_di_2 = "El Paso");
+
+			// SELECT OGR_FID, ASTEXT(SHAPE) AS POLYGON, hzdept_r AS top, hzdepb_r AS bottom, x.mukey, x.cokey, pi_r 
+			// FROM imsc.polygon AS p NATURAL JOIN imsc.chorizon_joins as x WHERE hzdept_r = 0 AND ST_INTERSECTS(ST_GEOMFROMTEXT(@geom1, 1), p.SHAPE);
+
+			// SELECT OGR_FID, ASTEXT(SHAPE) AS POLYGON, hzdept_r AS top, hzdepb_r AS bottom, x.mukey, x.cokey, pi_r 
+			// FROM imsc.polygon AS p NATURAL JOIN imsc.chorizon_joins as x WHERE hzdept_r = 0 AND ST_INTERSECTS(ST_GEOMFROMTEXT(@geom1, 1), p.SHAPE);
+
         	if ($district == "EverythingA") {
                 $query = "SELECT * from imsc.all_pi_48 ORDER BY OGR_FID DESC;";
                 // get file
